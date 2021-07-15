01abe36bf79fecc347a0881a5800db5a78b9a16c
jugaleezy <50313323+jugaleezy@users.noreply.github.com>
2021-07-15 13:11:57 +0400

    init commit

---
+++ Ownable.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+abstract contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Ownable: caller is not the owner");
+        _;
+    }
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    function getOwner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address _oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(_oldOwner, _owner);
+    }
+
+    /* create a function acceptOwnership where new owner has to accept ownership
+        after transfer ownership.
+    **/
+}
